01cab13df7d3620c20f05a8aa9c9ead5d89dc3cf
seven1106 <that9011@gmail.com>
2024-01-11 22:51:51 +0700

    Community List Flutter Drawer

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -2,38 +2,46 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:untitled/features/home/drawers/community_list_drawer.dart';
 
 import '../../auth/controller/auth_controller.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
+  void displayDrawer(BuildContext context) {
+    Scaffold.of(context).openDrawer();
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider);
     return Scaffold(
-        appBar: AppBar(
-      leading: IconButton(
-        icon: const Icon(Icons.menu),
-        onPressed: () {},
-      ),
-      title: Text('Home', style: GoogleFonts.poppins()),
-      actions: [
-        IconButton(
-          icon: const Icon(CupertinoIcons.search),
-          onPressed: () {},
-        ),
-        IconButton(
-          icon: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5.0),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage(user!.profilePic),
-
-            ),
+      drawer: const CommunityListDrawer(),
+      appBar: AppBar(
+        leading: Builder(builder: (context) {
+          return IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () => displayDrawer(context),
+          );
+        }),
+        title: Text('Home', style: GoogleFonts.poppins()),
+        actions: [
+          IconButton(
+            icon: const Icon(CupertinoIcons.search),
+            onPressed: () {},
           ),
-          onPressed: () {},
-        )
-      ],
-    ));
+          IconButton(
+            icon: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(user!.profilePic),
+              ),
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:routemaster/routemaster.dart';
+import 'package:untitled/features/community/screens/create_community_screen.dart';
 import 'package:untitled/features/home/screens/home_screen.dart';
 
 import 'features/auth/screens/login_screen.dart';
@@ -10,4 +11,5 @@ final loggedOutRoute = RouteMap(routes: {
 
 final loggedInRoute = RouteMap(routes: {
   '/': (_) => const MaterialPage<void>(child: HomeScreen()),
+  '/create-community': (_) => const MaterialPage<void>(child: CreateCommunityScreen()),
 });
