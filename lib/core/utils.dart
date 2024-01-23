792bdd9add28e782f970daab4e1878c3756bb20a
seven1106 <that9011@gmail.com>
2024-01-23 14:39:13 +0700

    pick image to post

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showSnackBar(BuildContext context, String message) {
   ScaffoldMessenger.of(context)
@@ -15,3 +18,37 @@ Future<FilePickerResult?> pickImage() async {
   );
   return result;
 }
+
+Future<List<XFile>?> pickMultipleImages() async {
+  try {
+    final pickedImages = await ImagePicker().pickMultiImage();
+    return pickedImages;
+  } catch (e) {
+    return null;
+  }
+}
+// Future<XFile?> pickImageCamera() async {
+//   try {
+//     final pickedImage = await ImagePicker().pickImage(source: ImageSource.camera);
+//     return pickedImage;
+//   } catch (e) {
+//     return null;
+//   }
+// }
+// Future<List<File>?> pickVideos() async {
+//   try {
+//     FilePickerResult? result = await FilePicker.platform.pickFiles(
+//       type: FileType.video,
+//       allowMultiple: true,
+//     );
+//
+//     if (result == null || result.files.isEmpty) {
+//       return null;
+//     }
+//     List<File> videoFiles = result.files.map((file) => File(file.path!)).toList();
+//     return videoFiles;
+//   } catch (e) {
+//     return null;
+//   }
+// }
+
--- lib/features/post/screens/add_post_screen.dart
+++ lib/features/post/screens/add_post_screen.dart
@@ -1,37 +1,110 @@
+import 'dart:io';
+
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AddPostScreen extends ConsumerWidget {
+import '../../../core/utils.dart';
+import '../../../theme/pallete.dart';
+
+class AddPostScreen extends ConsumerStatefulWidget {
   const AddPostScreen({super.key});
+  @override
+  ConsumerState createState() => _CreateAddPostScreenState();
+  }
+
+  class _CreateAddPostScreenState extends ConsumerState<AddPostScreen> {
+  final titleController = TextEditingController();
+  final bodyController = TextEditingController();
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  void dispose() {
+    super.dispose();
+    titleController.dispose();
+    bodyController.dispose();
+  }
+  List<File> imageFiles = [];
+  File? avatarFile;
+  void pickImages() async {
+    final res = await pickMultipleImages();
+    if (res != null) {
+      setState(() {
+        for (var pickedImage in res) {
+          imageFiles.add(File(pickedImage.path));
+        }
+      });
+    }
+  }
+  // void pickImageFromCamera() async {
+  //   final res = await pickImageCamera();
+  //   if (res != null) {
+  //     setState(() {
+  //         imageFiles.add(File(res.path));
+  //     });
+  //   }
+  // }
+  @override
+  Widget build(BuildContext context) {
+    final currentTheme = ref.watch(themeNotifierProvider);
+
     return Wrap(
       children: [
-        Container(
-          padding: const EdgeInsets.all(8.0),
-          child: const TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: 'What\'s on your mind?',
-            ),
-            maxLines: 5,
-          ),
-        ),
+    Container(
+      padding: const EdgeInsets.all(8.0),
+      child: const TextField(
+      decoration: InputDecoration(
+      hintText: 'What\'s on your mind?',
+      ),
+      maxLines: 5,
+      ),
+    ),
+        const SizedBox(height: 20),
+        // imageFiles.isNotEmpty
+        //     ? SizedBox(
+        //   height: 200,
+        //   child: GridView.builder(
+        //     scrollDirection: Axis.horizontal,
+        //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //       crossAxisCount: 1,
+        //     ),
+        //     itemCount: imageFiles.length,
+        //     itemBuilder: (BuildContext context, int index) {
+        //       return Padding(
+        //         padding: const EdgeInsets.all(8.0),
+        //         child: DottedBorder(
+        //           borderType: BorderType.RRect,
+        //           radius: const Radius.circular(10),
+        //           dashPattern: const [12, 4],
+        //           strokeCap: StrokeCap.round,
+        //           color: currentTheme.textTheme.bodyText2!.color!,
+        //           child: Container(
+        //             height: 200,
+        //             decoration: BoxDecoration(
+        //               borderRadius: BorderRadius.circular(10),
+        //             ),
+        //             width: double.infinity,
+        //             child: Image.file(imageFiles[index]),
+        //           ),
+        //         ),
+        //       );
+        //     },
+        //   ),
+        // )
+        //     : const SizedBox(height: 0),
         Container(
           padding: const EdgeInsets.all(8.0),
           child: Row(
             children: [
               IconButton(
                 icon: const Icon(Icons.image),
-                onPressed: () {},
+                onPressed: () =>pickImages(),
               ),
               IconButton(
                 icon: const Icon(Icons.linked_camera),
                 onPressed: () {},
               ),
               IconButton(
-                icon: const Icon(Icons.link),
+                icon: const Icon(Icons.video_collection),
                 onPressed: () {},
               ),
               IconButton(
@@ -42,7 +115,22 @@ class AddPostScreen extends ConsumerWidget {
                 icon: const Icon(Icons.more_horiz),
                 onPressed: () {},
               ),
+              const Spacer(),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                ),
+                child: const Text(
+                  'Post',
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              )
             ],
+
           ),
         ),
       ],);
