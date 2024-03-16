17daf911328dfe87ef548cf660098b113621620e
seven1106 <that9011@gmail.com>
2024-03-16 23:15:21 +0700

    toggleVideoPlayback

--- lib/features/short_video/screens/video_card.dart
+++ lib/features/short_video/screens/video_card.dart
@@ -14,13 +14,18 @@ class VideoPlayerItem extends StatefulWidget {
 
 class _VideoPlayerItemState extends State<VideoPlayerItem> {
   late VideoPlayerController videoPlayerController;
+  bool isVideoPlaying = true;
 
   @override
   void initState() {
     super.initState();
     videoPlayerController = VideoPlayerController.network(widget.videoUrl)
       ..initialize().then((value) {
-        videoPlayerController.play();
+        if (isVideoPlaying) {
+          videoPlayerController.play();
+        } else {
+          videoPlayerController.pause();
+        }
         videoPlayerController.setVolume(1);
       });
   }
@@ -31,17 +36,32 @@ class _VideoPlayerItemState extends State<VideoPlayerItem> {
     videoPlayerController.dispose();
   }
 
+  void toggleVideoPlayback() {
+    if (videoPlayerController.value.isPlaying) {
+      videoPlayerController.pause();
+    } else {
+      videoPlayerController.play();
+    }
+    setState(() {
+      isVideoPlaying = !isVideoPlaying;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return Container(
-      width: size.width,
-      height: size.height,
-      decoration: const BoxDecoration(
-        color: Colors.black,
+    return GestureDetector(
+      onTap: toggleVideoPlayback,
+      child: Container(
+        width: size.width,
+        height: size.height,
+        decoration: const BoxDecoration(
+          color: Colors.black,
+        ),
+        child: VideoPlayer(videoPlayerController),
       ),
-      child: VideoPlayer(videoPlayerController),
     );
   }
 }
+
